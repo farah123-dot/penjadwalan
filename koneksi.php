024cc59c0153d3f08ecbbe296320ddcd59558422
farah123-dot <Farahd479@gmail.com>
2026-08-03 18:19:26 +0700

    Update database connection to use environment variables

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,12 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_penjadwalan_kuliah";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal : " . mysqli_connect_error());
