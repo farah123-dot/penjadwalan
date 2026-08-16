935d4c3e154255e8f2a1f1b5837060670571694e
farah123-dot <Farahd479@gmail.com>
2026-08-16 20:47:01 +0700

    Implement user login functionality

--- auth/proses_login.php
+++ auth/proses_login.php
@@ -1,1 +1,40 @@
+<?php
 
+session_start();
+
+require_once "koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = mysqli_query($conn, "
+    SELECT *
+    FROM user
+    WHERE username='$username'
+    AND password='$password'
+");
+
+if (mysqli_num_rows($query) > 0) {
+
+    $data = mysqli_fetch_assoc($query);
+
+    $_SESSION['login'] = true;
+
+    $_SESSION['id_user'] = $data['id_user'];
+    $_SESSION['nama'] = $data['nama'];
+    $_SESSION['role'] = $data['role'];
+    $_SESSION['id_dosen'] = $data['id_dosen'];
+
+    header("Location: index.php");
+    exit;
+
+} else {
+
+    echo "
+    <script>
+        alert('Username atau Password salah');
+        window.location='login.php';
+    </script>
+    ";
+
+}
