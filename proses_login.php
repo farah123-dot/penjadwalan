4c331ff0928dd68c9e0c360e73aefe60fc726fb2
farah123-dot <Farahd479@gmail.com>
2026-08-16 20:19:02 +0700

    Update login process to include 'id_dosen' session
    Added session variable 'id_dosen' to store user data.

--- proses_login.php
+++ proses_login.php
@@ -4,43 +4,37 @@ session_start();
 
 require_once "koneksi.php";
 
-$username=$_POST['username'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$password=$_POST['password'];
-
-$query=mysqli_query($conn,"
-SELECT *
-FROM user
-WHERE username='$username'
-AND password='$password'
+$query = mysqli_query($conn, "
+    SELECT *
+    FROM user
+    WHERE username='$username'
+    AND password='$password'
 ");
 
-if(mysqli_num_rows($query)>0){
+if (mysqli_num_rows($query) > 0) {
 
-$data=mysqli_fetch_assoc($query);
+    $data = mysqli_fetch_assoc($query);
 
-$_SESSION['login']=true;
+    $_SESSION['login'] = true;
 
-$_SESSION['id_user']=$data['id_user'];
+    $_SESSION['id_user'] = $data['id_user'];
+    $_SESSION['nama'] = $data['nama'];
+    $_SESSION['role'] = $data['role'];
+    $_SESSION['id_dosen'] = $data['id_dosen'];
 
-$_SESSION['nama']=$data['nama'];
+    header("Location: index.php");
+    exit;
 
-$_SESSION['role']=$data['role'];
+} else {
 
-header("Location:index.php");
-
-}else{
-
-echo "
-
-<script>
-
-alert('Username atau Password salah');
-
-window.location='login.php';
-
-</script>
-
-";
+    echo "
+    <script>
+        alert('Username atau Password salah');
+        window.location='login.php';
+    </script>
+    ";
 
 }
