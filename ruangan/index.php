382502b7fb9a3b0b6e8a77db46fe47d6ec460215
farah123-dot <Farahd479@gmail.com>
2026-08-19 22:54:05 +0700

    Update index.php

--- ruangan/index.php
+++ ruangan/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../koneksi.php";
 require_once "../auth/cek_login.php";
 
 include "../layout/header.php";
-include "../layout/sidebar.php";
+include "../layout/sidebar_admin.php";
 
 $query = mysqli_query($conn, "SELECT * FROM ruangan ORDER BY id_ruangan ASC");
 
