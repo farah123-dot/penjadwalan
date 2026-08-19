b5073cd65669f8bc6819e69553977ba1a83ee2cf
farah123-dot <Farahd479@gmail.com>
2026-08-19 23:14:25 +0700

    Update index.php

--- kelas_dibuka/index.php
+++ kelas_dibuka/index.php
@@ -1,1 +1,18 @@
+<?php
 
+require_once "../koneksi.php";
+require_once "../auth/cek_login.php";
+
+include "../layout/header.php";
+include "../layout/sidebar_admin.php";
+?>
+
+<div class="container-fluid">
+    <h3>Data Kelas Dibuka</h3>
+
+    <p>Halaman pengelolaan kelas yang dibuka untuk perkuliahan.</p>
+</div>
+
+<?php
+include "../layout/footer.php";
+?>
